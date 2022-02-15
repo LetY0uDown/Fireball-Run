e90774a3189563ad29b5b8ab82ad53ac55d7f0ef
Максим Манин <maks.manin.04@gmail.com>
2022-02-15 17:21:46 +1000

    Добавлено сохранение персонального рекорда Изменён размер окна Изменён интерфейс

--- Fireball Run/MainWindow.xaml.cs
+++ Fireball Run/MainWindow.xaml.cs
@@ -6,6 +6,10 @@ using System.Windows.Controls;
 using System.Windows.Threading;
 using System.Collections.Generic;
 using System.Windows.Media.Imaging;
+using System.IO;
+using System.Xml.Serialization;
+using System.Threading.Tasks;
+using System.Windows.Media;
 
 namespace Fireball_Run
 {
@@ -22,26 +26,36 @@ namespace Fireball_Run
             timer.Tick += Timer_Tick;
         }
 
-        private const int ENEMY_START_POS = 610;
-        private const int GAME_FIELD_HEIGHT = 90;
+        private readonly int FIREBALL_START_POS = 810;
+        private const int GAME_FIELD_HEIGHT = 100;
 
         private const int FIREBALL_WIDTH = 60;
         private const int FIREBALL_HEIGHT = 50;
 
         private const int CHARACTER_POS = 50;
 
-        private int enemySpeed = 2;
+        private int enemySpeed
+        {
+            get
+            {
+                if (2 + scores / 1500 >= 5)
+                    return 5;
+                else
+                    return 2 + scores / 1500;
+            }
+        }
 
         private int scores = 0;
+        private int personalBest = 0;
 
-        private List<Entity> enemies = new();
+        private List<Entity> fireballs = new();
 
         private DispatcherTimer timer = new();
         private DispatcherTimer jumpTimer = new();
 
-        private Random random = new();
+        private readonly Random random = new();
 
-        private Entity character = new()
+        private readonly Entity character = new()
         {
             Body = new Image
             {
@@ -68,19 +82,28 @@ namespace Fireball_Run
             Canvas.SetRight(GameField.Children[0], 20);
 
             scores = 0;
-            enemies.Clear();
+            fireballs.Clear();
             GameField.Children.Add(character.Body);
 
             Canvas.SetBottom(character.Body, GAME_FIELD_HEIGHT);
             Canvas.SetLeft(character.Body, CHARACTER_POS);
 
-            SpawnEnemy();
+            tbNewRecord.Visibility = Visibility.Collapsed;
 
             timer.Start();
         }
         private void StopGame()
         {
             GameField.Children.Clear();
+
+            tbTitle.Text = "You lose!";
+            tbTitle.Foreground = Brushes.Red;
+
+            if (scores > personalBest)
+            {
+                SaveRecord();
+                tbNewRecord.Visibility = Visibility.Visible;
+            }
 
             MenuPanel.Visibility = Visibility.Visible;
 
@@ -89,16 +112,21 @@ namespace Fireball_Run
 
         private void Timer_Tick(object? sender, EventArgs e)
         {
-            if (random.Next(0, 500) == 0 || enemies.Count == 0)
+            if (fireballs.Count == 0 || CanEnemySpawn())
                 SpawnEnemy();
 
-            foreach (var enemy in enemies.ToList())
+            foreach (var enemy in fireballs.ToList())
             {
                 MoveEnemy(enemy);
                 CollisionCheck(enemy);
             }
 
             tbScores.Text = $"Score: {scores++}";
+
+            if (scores > personalBest && scores < personalBest + 500)
+                tbNewRecord.Visibility = Visibility.Visible;
+            else
+                tbNewRecord.Visibility = Visibility.Collapsed;
         }
 
         private void CollisionCheck(Entity enemy)
@@ -119,9 +147,13 @@ namespace Fireball_Run
                 RemoveEnemy(enemy);
         }
 
+        private bool CanEnemySpawn()
+        {
+            return Canvas.GetLeft(fireballs[^1].Body) < FIREBALL_START_POS - 210 && random.Next(0, 300) == 0;
+        }
         private void SpawnEnemy()
         {
-            enemies.Add(new Entity
+            fireballs.Add(new Entity
             {
                 Body = new Image
                 {
@@ -131,20 +163,20 @@ namespace Fireball_Run
                 },
                 Position = new()
                 {
-                    X = ENEMY_START_POS,
+                    X = FIREBALL_START_POS,
                     Y = GAME_FIELD_HEIGHT
                 }
             });
 
-            GameField.Children.Add(enemies[^1].Body);
+            GameField.Children.Add(fireballs[^1].Body);
 
-            Canvas.SetBottom(enemies[^1].Body, GAME_FIELD_HEIGHT);
-            Canvas.SetLeft(enemies[^1].Body, ENEMY_START_POS);
+            Canvas.SetBottom(fireballs[^1].Body, GAME_FIELD_HEIGHT);
+            Canvas.SetLeft(fireballs[^1].Body, FIREBALL_START_POS);
 
         }
         private void RemoveEnemy(Entity enemy)
         {
-            enemies.Remove(enemy);
+            fireballs.Remove(enemy);
             GameField.Children.Remove(enemy.Body);
         }
 
@@ -185,6 +217,45 @@ namespace Fireball_Run
         private void ExitImage_MouseDown(object sender, MouseButtonEventArgs e)
         {
             Application.Current.Shutdown();
+        }
+
+        private void LoadRecord()
+        {
+            if (File.Exists("personalBest.xml"))
+            {
+                XmlSerializer? serializer = new(typeof(int));
+
+                using (Stream reader = new FileStream("personalBest.xml", FileMode.Open))
+                {
+                    personalBest = (int)serializer.Deserialize(reader);
+                }
+
+                tbPersonalBest.Text = $"Personal Best: {personalBest}";
+            }
+        }
+
+        private async void SaveRecord()
+        {
+            XmlSerializer serializer = new(typeof(int));
+
+            personalBest = scores;
+
+            using (Stream writer = new FileStream("personalBest.xml", FileMode.Create))
+            {
+                await Task.Run(() => serializer.Serialize(writer, personalBest));
+            }
+
+            tbPersonalBest.Text = $"Personal Best: {personalBest}";
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadRecord();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
         }
     }
 }
